86b106d00e4767bea56d950287c3d5c5d5dfbfef
Ethan Nguyen <ethan.nguyen0311@gmail.com>
2022-04-02 20:40:36 -0400

    modify redirection

--- backend/routes/itemRouter.js
+++ backend/routes/itemRouter.js
@@ -30,10 +30,16 @@ function createItem(status, item, description, negotiation, price, method_of_del
 
 // get all items (non-users can view also)
 router.get('/', (req, res, next) => {
+    if (req.headers['authorization'])
+        console.log('logged in');
+    else{
+        console.log("not logged in");
+    }
     Item.find({}).populate('OP').exec((err, results) => {
         if (err){
             return next(err);
         } else{
+            console.log("user's id: " + req.user);
             res.send(results);
         }
     });
@@ -58,7 +64,7 @@ router.get('/load-data', (req, res, next) => {
                     createItem('available', 'm1 chip macbook air 2021', 'Need money quick', false, '998', 'drop-off', users[1], callback);
                 },
                 function(callback){
-                    createItem('sold', 'PS4', "still works. need to buy because I bought a PS5", true, '300', 'delivery', users[2], callback);
+                    createItem('sold', 'PS4', "still works. need to sell because I bought a PS5", true, '300', 'delivery', users[2], callback);
                 }
             ]);
             res.send("done");
@@ -81,6 +87,35 @@ router.get("/create", (req, res, next) => {
     res.send("you will submit information for a new item here");
 });
 
-router.post('/')
+// posting a new item
+// make sure to store the user's id in request headers in the id key
+router.post('/create', [
+    body('item', "Item name must not be empty").isLength({min: 5}).trim().escape(), 
+    body("description", "Description of item must not be empty").isLength({min: 5}).trim().escape(),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()){ // errors encountered while valiadting data
+            res.status(403).send((errors.array())[0].msg);
+        }
+        else{
+            let newItem = new Item({
+                status: req.body.status, 
+                item: req.body.item.replace(/&#x27;/g, "'").replace(/&quot;/g, '"').replace(/&amp;/g, '&').replace(/&lt;/g, "<").replace(/&gt;/g, ">"), 
+                price: req.body.price,
+                description: req.body.description.replace(/&#x27;/g, "'").replace(/&quot;/g, '"').replace(/&amp;/g, '&').replace(/&lt;/g, "<").replace(/&gt;/g, ">"), 
+                willing_to_negotiate: req.body.negotiation,
+                method_of_delivery: req.body.delivery,
+                OP: req.headers['id']
+            });
+            newItem.save((err) => {
+                if (err)
+                    return next(err);
+                else{
+                    // use fs to add images into local folder
+                }
+            })
+        }
+    }
+]);
 
 module.exports = router;
